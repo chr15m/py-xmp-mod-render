c6def12ea629d7d1ee5bfd0a46ac65e53d3f0f29
Chris McCormick <chris@mccormick.cx>
2023-05-10 07:01:22 +0800

    Add a setup.py to make it installable.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='XmpModRender',
+    version='0.0.1',
+    url='https://github.com/chr15m/py-xmp-mod-render.git',
+    author='Chris McCormick',
+    author_email='chris@mccormick.cx',
+    description='Render tracker modules with XMP.',
+    packages=find_packages(),    
+    install_requires=[],
+)
